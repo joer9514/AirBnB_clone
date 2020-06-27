3150f8e4eb66201639b0f798b0352fbfcdcef705
anhujuju <anhujuju1@gmail.com>
2020-06-27 17:26:29 +0000

    base_model_update

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,46 @@
 #!/usr/bin/python3
-"""The base model"""
+"""Class BaseModel"""
 
+import json
 import uuid
 import models
 from datetime import datetime
 
-
 class BaseModel:
-    """The base model"""
-
+     """Constructor"""
     def __init__(self, *args, **kwargs):
-        """The constructor method"""
-        pass
+        """Attributes of the BaseModel class"""
+        if args is not None and len(args) > 0:
+            pass
+        if kwargs:
+            for key, item in kwargs.items():
+                if key in ['create_at', 'update_at']:
+                    item = datetime.strptime(item, "%Y-%m-%dT%H:%M:%S.%f")
+                if key not in ['__class__']:
+                    setattr(self, key, item)
+        else:
+            self.id = str(uuid.uuid4())
+            self.create_at = self.update_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """String Literal"""
+        a, b, c = self.__class__.__name__, self.id, self.__dict__
+        return("[{}] ({}) {}".format(a, b, c))
+
+    def save(self):
+        """updates the public instance attribute updated_at with the current datetime"""
+        self.update_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
+
+    def to_dict(self):
+        """Returns a dictionay"""
+        my_dict = {}
+        for key, item in self.__dict__.items():
+            if key in ['create_at', 'update_at']:
+                my_dict[key] = item.isoformat()
+            else:
+                my_dict[key] = item
+        my_dict['__class__'] = self.__class__.__name__
+        return my_dict
