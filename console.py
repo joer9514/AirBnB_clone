39f8d83061f4f90d2be53f1b1d1b55af9e04fed7
anhujuju <anhujuju1@gmail.com>
2020-06-29 15:41:19 +0000

    update console

--- console.py
+++ console.py
@@ -2,18 +2,30 @@
 """The console simulation"""
 
 import cmd
+import sys
+import json
+import shlex
+from models import amenity
+from models.base_model import BaseModel
+from models import city
+from models import place
+from models import state
+from models import review
+from models.user import User
+
 
 
 class HBNBCommand(cmd.Cmd):
     """HBNB"""
-    prompt = '(hbnb) '
-    classes = {"user",
+    prompt = ' start (hbnb) '
+    group = {"user",
                "BaseModel",
                "place",
                "State",
                "Amenity",
                "City",
                "Review"}
+    
 
     def do_EOF(self, inputline):
         """Exit console"""
@@ -22,6 +34,28 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, inputline):
         """Quit command to exit console"""
         return True
+    err_list = ["** class name missing **", "** class doesn't exist **"]
+
+    def err_msg(self, n):
+        """Return error messages"""
+        msg_dict = {1: "** class name missing **",
+                    2: "** class doesn't exist **",
+                    }
+        for key, item in msg_dict.items():
+            if key == n:
+                print(item)
+
+    def _create(self, arg):
+        """Used to create a new instance of BaseModel and saves
+        the instance to a JSON file"""
+        if arg == "":
+            print(self.err_list[0])
+        elif arg not in self.group:
+            print(self.err_list[1])
+        else:
+            arg = eval(arg)()
+            arg.save()
+            print(arg.id)
 
 
 if __name__ == '__main__':
