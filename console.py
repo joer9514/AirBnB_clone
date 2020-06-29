55dfe55bac2af105bda0b159e6b33618cb4d1587
anhujuju <anhujuju1@gmail.com>
2020-06-29 16:39:49 +0000

    update console add show

--- console.py
+++ console.py
@@ -4,12 +4,14 @@
 import cmd
 import sys
 import json
+import models
 import shlex
 from models import amenity
 from models.base_model import BaseModel
 from models import city
 from models import place
 from models import state
+from models import storage
 from models import review
 from models.user import User
 
@@ -25,7 +27,6 @@ class HBNBCommand(cmd.Cmd):
                "Amenity",
                "City",
                "Review"}
-    
 
     def do_quit(self, line):
         """Quit command to exit the program"""
@@ -34,12 +35,15 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         """Executes the EOF (Ctrl -D/ Ctrl-Z) commands on console"""
         return True
-    err_list = ["** class name missing **", "** class doesn't exist **"]
+    err_list =     err_list = ["** class name missing **", "** class doesn't exist **",
+                    "** instance id missing **", "** no instance found **",]
 
     def err_msg(self, n):
         """Return error messages"""
         msg_dict = {1: "** class name missing **",
                     2: "** class doesn't exist **",
+                    3: "** instance id missing **",
+                    4: "** no instance found **",
                     }
         for key, item in msg_dict.items():
             if key == n:
@@ -56,6 +60,25 @@ class HBNBCommand(cmd.Cmd):
             arg = eval(arg)()
             arg.save()
             print(arg.id)
+    
+    def do_show(self,line):
+        """Print string representation of instance"""
+        arg = line.split()
+        if line == "":
+            print(self.err_list[0])
+        elif arg[0] not in self.group:
+            print(self.err_list[1])
+        elif len(arg) < 2:
+            print(self.err_list[2])
+        else:
+            data_dump = models.storage.all()
+            key = "{}.{}".format(arg[0], arg[1])
+            try:
+                object = data_dump[key]
+                print(object)
+            except KeyError:
+                print(self.err_list[3])
+      
             
 
 if __name__ == '__main__':
